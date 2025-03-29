92d55c11ea96a7b8d69ce1578f8956eb16d1bb83
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2025-03-29 15:10:15 -0300

    feat: add dependencies in clients controller

--- lib/config/main/main.dart
+++ lib/config/main/main.dart
@@ -2,16 +2,35 @@ part of '../config.dart';
 
 final connection = PostgreSQL();
 
+final getClientByIdUseCase = GetClientByIdService(
+  getClientByIdGateway: GetClientByIdDAO(connection),
+);
+
+final getClientsUseCase = GetClientsService(
+  getClientsGateway: GetClientsDAO(connection: connection),
+);
+
+final addClientUseCase = AddClientService(
+  addClientGateway: AddClientDAO(connection),
+  getClientByEmailGateway: GetClientByEmailDAO(connection),
+);
+
+final deleteClientUseCase = DeleteClientService(
+  deleteClientGateway: DeleteClientDAO(connection),
+  getClientByIdUseCase: getClientByIdUseCase,
+);
+
+final updateClientUseCase = UpdateClientService(
+  updateClientGateway: UpdateClientDAO(connection),
+  getClientByIdUseCase: getClientByIdUseCase,
+);
+
 final controllers = <Controller>[
-  // ClientsController(
-  //   getClientsUseCase: GetClientsService(
-  //     getClientsGateway: GetClientsDAO(
-  //       connection: connection,
-  //     ),
-  //   ),
-  //   addClientUseCase: AddClientService(
-  //     addClientGateway: AddClientDAO(connection),
-  //     getClientByEmailGateway: GetClientByEmailDAO(connection),
-  //   ),
-  // ),
+  ClientsController(
+    getClientsUseCase: getClientsUseCase,
+    addClientUseCase: addClientUseCase,
+    getClientByIdUseCase: getClientByIdUseCase,
+    deleteClientUseCase: deleteClientUseCase,
+    updateClientUseCase: updateClientUseCase,
+  )
 ];
